b9e7116ea8ddd21e12ae842bfad1259f77527fbc
andrew <andrew@calc>
2015-06-02 23:57:27 +0400

    andrew commit

---
+++ src/devicePlot.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   devicePlot.cpp
+ * Author: andrew
+ * 
+ * Created on 2 Июнь 2015 г., 23:56
+ */
+
+#include "devicePlot.h"
+
+devicePlot::devicePlot() {
+}
+
+devicePlot::devicePlot(const devicePlot& orig) {
+}
+
+devicePlot::~devicePlot() {
+}
+
---
+++ src/devicePlot.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   devicePlot.h
+ * Author: andrew
+ *
+ * Created on 2 Июнь 2015 г., 23:56
+ */
+
+#ifndef DEVICEPLOT_H
+#define	DEVICEPLOT_H
+
+class devicePlot {
+public:
+    devicePlot();
+    devicePlot(const devicePlot& orig);
+    virtual ~devicePlot();
+private:
+
+};
+
+#endif	/* DEVICEPLOT_H */
+
