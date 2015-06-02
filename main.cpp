26dece968c41000417ad672097803dcc189f6006
andrew <andrew@calc>
2015-06-02 23:41:17 +0400

    master commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: andrew
+ *
+ * Created on 2 Июнь 2015 г., 23:37
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
